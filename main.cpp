5f1230ce15a163868e8f35f475394adc3f8f7854
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-11-25 19:47:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,15 @@ int main()
 	std::cin >> str1 >> symbol >> str2;
 	if (symbol == '+' && str1[0] != '-' && str2[0] != '-')
 		std::cout << add(str1, str2);
-	if(symbol == '-' && str1[0] == '-' && str2[0] == '-')
+	if (symbol == '-' && str1[0] == '-' && str2[0] == '-') 
 	{
-		std::string a, b;
+		std::string a;
 		for (int i = 1; i < len(str1); i++)
 			a += str1[i];
-		for (int i = 1; i < len(str2); i++)
-			b += str1[i];
-		std::cout << add(a, b);
+		std::string f = dec(a, str2);
+		f = reverce(f);
+		f += '-';
+		std::cout << reverce(f);
 	}
 	if(symbol == '-' && str1[0] != '-' && str2[0] == '-')
 	{
