efb9e4323a16efebc6ed697cc7aa9d1420403b7c
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-11-28 21:07:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 	std::cin >> str1 >> symbol >> str2;
 	if (symbol == '+' && str1[0] != '-' && str2[0] != '-')
 		std::cout << add(str1, str2);
+	//done
 	if (symbol == '-' && str1[0] == '-' && str2[0] == '-') 
 	{
 		std::string a;
@@ -17,26 +18,28 @@ int main()
 		f += '-';
 		std::cout << reverce(f);
 	}
+	//done
 	if(symbol == '-' && str1[0] != '-' && str2[0] == '-')
-	{
-		std::string a;
-		for (int i = 1; i < len(str2); i++)
-			a += str2[i];
-		std::cout << add(str1, a);
-	}
+		std::cout << dec(str1, str2);
+	//done
 	if (symbol == '-' && str1[0] == '-' && str2[0] != '-')
 	{
 		std::string a;
 		for (int i = 1; i < len(str1); i++)
 			a += str1[i];
-		std::cout << add(a, str2);
+		std::string f = add(a, str2);
+		f = reverce(f);
+		f += '-';
+		std::cout << reverce(f);
 	}
+	//done
 	if (symbol == '-' && str1[0] != '-' && str2[0] != '-')
 	{
 		str2 = reverce(str2);
 		str2 += '-';
 		std::cout << dec(str1, reverce(str2));
 	}
+	//done
 	if (symbol == '+' && str1[0] != '-' && str2[0] == '-')
 		std::cout << dec(str1, str2);
 	if (symbol == '+' && str1[0] == '-' && str2[0] != '-')
